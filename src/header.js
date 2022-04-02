c34e3b635e477918534aa67e94cce73772736a33
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-02 08:08:49 +0100

    adding faqs and terms

---
+++ src/header.js
@@ -0,0 +1,7 @@
+document.getElementById("register").onclick = function () {
+    window.location.href = "../signIn_and_signUp/signUp.html";
+  };
+  
+  document.getElementById("signIn").onclick = function () {
+    window.location.href = "../signIn_and_signUp/signIn.html";
+  };
